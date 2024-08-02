9b87970c056b5805a679669282bfe845b18ae904
2252942@tongji.edu.cn <2252942@tongji.edu.cn>
2024-08-02 19:39:32 +0800

    commit lab03

---
+++ LadPageTables/kernel/sysproc.c
@@ -0,0 +1,133 @@
+#include "types.h"
+#include "riscv.h"
+#include "param.h"
+#include "defs.h"
+#include "date.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+
+uint64
+sys_exit(void)
+{
+  int n;
+  if(argint(0, &n) < 0)
+    return -1;
+  exit(n);
+  return 0;  // not reached
+}
+
+uint64
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+uint64
+sys_fork(void)
+{
+  return fork();
+}
+
+uint64
+sys_wait(void)
+{
+  uint64 p;
+  if(argaddr(0, &p) < 0)
+    return -1;
+  return wait(p);
+}
+
+uint64
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+uint64
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+
+#ifdef LAB_PGTBL
+int
+sys_pgaccess(void)
+{
+  // lab pgtbl: your code here.
+  uint64 base;
+  int len;
+  uint64 mask;
+  uint bitmask = 0;
+  pte_t* pte;
+
+  if (argaddr(0, &base) < 0 || argint(1, &len) < 0 || argaddr(2, &mask) < 0)
+    return -1;
+  if (len > 32)   // pages to be checked should be no more than 20
+    len = 32;
+
+  for (int i = 0; i < len; i++) 
+  {
+    pte = walk(myproc()->pagetable, base + PGSIZE * i, 0);
+    if (*pte & PTE_A)   // access bit is valid
+    {
+      *pte &= ~PTE_A;       // eliminate access bit
+      bitmask |= (1 << i);  // set bitmask
+    }
+  }
+
+  if (copyout(myproc()->pagetable, mask, (char*)&bitmask, sizeof(bitmask)) < 0)   // copy the result back to user space
+    return -1;
+
+  return 0;
+}
+#endif
+
+uint64
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+uint64
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
